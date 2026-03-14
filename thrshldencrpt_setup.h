8ebfadbc6dfa70cab5c3a39d91165764698ba7ed
drmike8888 <eresrch@eskimo.com>
2026-03-14 17:08:12 -0500

    reference to mdots routine

--- thrshldencrpt_setup.h
+++ thrshldencrpt_setup.h
@@ -46,6 +46,7 @@ int mod_matinv(mpz_t *inv, long n, mpz_t *mat, mpz_t prime);
 int mod_matmul(mpz_t *c, mpz_t *a, long m, long n,
 	       mpz_t *b, long r, long s, mpz_t prime);
 void genshare(mpz_t *share, long N, mpz_t mod);
+void mdots(mpz_t ms, long row, mpz_t *s_vec, mpz_t *shrmtrx, long N, mpz_t mod);
 void zvcalc(mpz_t *cvec, CRS *crs, mpz_t *shrmtrx, mpz_t c, mpz_t *s_vec, long N);
 void ytaucalc(CRS *crs, mpz_t *cvec, long n, long N);
 void cgcalc(CRS *crs, mpz_t *cvec, long N);
